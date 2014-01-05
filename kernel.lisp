3070e4673ad2268a5b3b8d2927fc17ade92f11fa
David Albert <davidbalbert@gmail.com>
2014-01-05 18:40:19 -0500

    defn => defun

--- kernel.lisp
+++ kernel.lisp
@@ -1,18 +1,18 @@
-(defmacro defn (name arg-names & bodies)
+(defmacro defun (name arg-names & bodies)
   `(def ,name (lambda ,arg-names ,@bodies)))
 
-(defn list (& args) args)
+(defun list (& args) args)
 
-(defn nil? (x) (= x nil))
+(defun nil? (x) (= x nil))
 
-(defn empty? (x) (= x nil))
+(defun empty? (x) (= x nil))
 
-(defn atom? (x) (not (list? x)))
+(defun atom? (x) (not (list? x)))
 
-(defn not (x)
-      (if (= x nil)
-        t
-        nil))
+(defun not (x)
+  (if (= x nil)
+    t
+    nil))
 
 (defmacro comment (& exprs)
   nil)
@@ -25,25 +25,25 @@
 (defmacro do (expr & exprs)
   `(let () ,expr ,@exprs))
 
-(defn map (f coll)
-      (unless (empty? coll)
-        (cons (f (car coll)) (map f (cdr coll)))
-        ()))
+(defun map (f coll)
+  (unless (empty? coll)
+    (cons (f (car coll)) (map f (cdr coll)))
+    ()))
 
-(defn filter (pred coll)
-      (unless (empty? coll)
-        (if (pred (car coll))
-          (cons (car coll) (filter pred (cdr coll)))
-          (filter pred (cdr coll)))
-        ()))
+(defun filter (pred coll)
+  (unless (empty? coll)
+    (if (pred (car coll))
+      (cons (car coll) (filter pred (cdr coll)))
+      (filter pred (cdr coll)))
+    ()))
 
-(defn reduce (f acc coll)
-      (unless (empty? coll)
-        (reduce f (f acc (car coll)) (cdr coll))
-        acc))
+(defun reduce (f acc coll)
+  (unless (empty? coll)
+    (reduce f (f acc (car coll)) (cdr coll))
+    acc))
 
-(defn comp (f g)
-      (lambda (x) (f (g x))))
+(defun comp (f g)
+  (lambda (x) (f (g x))))
 
 (defmacro let (bindings expr & exprs)
   ((lambda (names vals)
@@ -57,6 +57,6 @@
     (let ((name (car (car bindings)))
           (value (car (cdr (car bindings)))))
 
-         `((lambda (,name)
-             (let* ,(cdr bindings) ,expr ,@exprs))
-           ,value))))
+      `((lambda (,name)
+          (let* ,(cdr bindings) ,expr ,@exprs))
+        ,value))))
